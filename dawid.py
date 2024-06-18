8d010f336dc6fd87eab2af6f9db12f6573974a04
summer24 <summer24@fwunuc139.ad.fz-rossendorf.de>
2024-06-18 16:28:00 +0200

    add dgsm calculations

--- dawid.py
+++ dawid.py
@@ -1,4 +1,5 @@
 import numpy as np
+import minterpy as mp
 
 
 domain = np.array([
@@ -37,3 +38,54 @@ def translate_interval(a, b, xx):
     scale = b - avg
     
     return xx * scale + avg
+
+
+def get_nwt_interpolant(f, spatial_dimension=6, poly_degree=8, lp_degree=1.0):
+    
+    mi = mp.MultiIndexSet.from_degree(
+        spatial_dimension=spatial_dimension,
+        poly_degree=poly_degree,
+        lp_degree=lp_degree
+    )
+
+    grd = mp.Grid(mi)
+    lag_coeffs = f(grd.unisolvent_nodes)
+
+    lag_poly = mp.LagrangePolynomial(
+        multi_index=mi,
+        coeffs=lag_coeffs,
+    )
+    
+    return mp.LagrangeToNewton(lag_poly)()
+    
+    
+def dgsm(nwt_poly, i, variance, spatial_dimension=6):
+    nwt_diff = nwt_poly.partial_diff(i)
+    interval_length = domain[i][1] - domain[i][0]
+    diff_const = np.square(interval_length / 2)
+    vi = (nwt_diff * nwt_diff).integrate_over() * diff_const * np.power(1/2, spatial_dimension)
+    dgsm_const = np.square(interval_length / np.pi) / variance
+    return dgsm_const * vi
+
+
+def compute_dgsm(nwt_poly, variance):
+    spatial_dimension = domain.shape[0]
+    dgsm_vals = np.zeros(spatial_dimension, dtype=np.float64)
+    for i in range(spatial_dimension):
+        dgsm_vals[i] = dgsm(nwt_poly, i, variance)
+        
+    return dgsm_vals
+    
+    
+def visualize_dgsm(dgsm_values):
+    plt.bar(["R_b1", "R_b2", "R_f", "R_c1", "R_c2", "beta"], dgsm_values)
+    plt.ylabel("DGSM")
+    plt.xlabel("OTL parameters")
+    plt.yscale("log")
+    plt.show()
+    
+def run_dgsm():
+    variance_value = 0.6095304683143756
+    nwt_poly = get_nwt_interpolant(otl_normalized, spatial_dimension=domain.shape[0], poly_degree=8, lp_degree=1)
+    D = compute_dgsm(nwt_poly, variance_value)
+    visualize_dgsm(D)
